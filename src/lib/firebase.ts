4a458ff8014b5a93ded419220495a01924473c82
Antoinette <thompsonanthoinette@gmail.com>
2026-04-15 20:06:57 -0500

    added env config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDclshn6LN1qfYv82p1mHAi90GoWZ04TdY",
-  authDomain: "islandtech-booking.firebaseapp.com",
-  projectId: "islandtech-booking",
-  storageBucket: "islandtech-booking.firebasestorage.app",
-  messagingSenderId: "1045309843527",
-  appId: "1:1045309843527:web:9c99242bdcceaeabdea962",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
